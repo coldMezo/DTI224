5c0892e00b7441df7385c51b4bb8be3592c37f38
coldMezo <mrpiing16@gmail.com>
2026-08-18 22:14:15 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,42 @@
-const CACHE_NAME = "mazin-app-v1";
+const CACHE_NAME = "mazin-app-v2";
 
 const FILES_TO_CACHE = [
     "./",
     "./index.html",
     "./hobby.html",
+    "./change.html",
+    "./grading.html",
+    "./converter.html",
+    "./distance.html",
     "./style.css",
     "./manifest.json"
 ];
 
+// Install Event
 self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(() => self.skipWaiting())
     );
 });
 
+// Activate Event (حذف الكاش القديم عند التحديث)
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.map(key => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+// Fetch Event
 self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request)
